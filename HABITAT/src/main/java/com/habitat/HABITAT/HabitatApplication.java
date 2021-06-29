612d882c2d0e503b4f6cfdeb52a701d4daf777cf
fegiacomelli <69607844+fegiacomelli@users.noreply.github.com>
2021-06-29 09:03:00 -0300

    Att 1.0.0

---
+++ HABITAT/src/main/java/com/habitat/HABITAT/HabitatApplication.java
@@ -0,0 +1,13 @@
+package com.habitat.HABITAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitatApplication.class, args);
+	}
+
+}
