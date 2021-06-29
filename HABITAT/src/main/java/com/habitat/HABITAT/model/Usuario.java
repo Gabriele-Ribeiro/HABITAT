1680bdbb03c6efb34f4ce584b2c630d8b58e17d3
fegiacomelli <69607844+fegiacomelli@users.noreply.github.com>
2021-06-29 12:06:54 -0300

    add Import Email

--- HABITAT/src/main/java/com/habitat/HABITAT/model/Usuario.java
+++ HABITAT/src/main/java/com/habitat/HABITAT/model/Usuario.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
