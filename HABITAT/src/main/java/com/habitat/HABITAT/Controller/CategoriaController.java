48b3b494ebbe1267c5339eb13ede2db3cd06d6f9
thomazfranca <trsf1990@gmail.com>
2021-07-02 10:36:38 -0300

    Criando Get and Post

--- HABITAT/src/main/java/com/habitat/HABITAT/Controller/CategoriaController.java
+++ HABITAT/src/main/java/com/habitat/HABITAT/Controller/CategoriaController.java
@@ -3,10 +3,13 @@ package com.habitat.HABITAT.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,25 +19,38 @@ import com.habitat.HABITAT.model.Categoria;
 @RequestMapping("/categoria")
 @CrossOrigin("*")
 public class CategoriaController {
-	
+
 	@Autowired
 	private com.habitat.HABITAT.Repositories.CategoriaRepository repository;
-	
+
 	@GetMapping("/todos")
-	public ResponseEntity<List<Categoria>> buscarTodos(){
+	public ResponseEntity<List<Categoria>> buscarTodos() {
 		List<Categoria> listaCategoria = repository.findAll();
 		if (listaCategoria.isEmpty()) {
 			return ResponseEntity.status(204).build();
-		}
-		else {
+		} else {
 			return ResponseEntity.status(200).body(listaCategoria);
 		}
 	}
 
 	@GetMapping("/id/{id}")
-	public ResponseEntity<Categoria> buscarId(@PathVariable long id){
-		return repository.findById(id)
-				.map(resp -> ResponseEntity.ok(resp))
-				.orElse(ResponseEntity.notFound().build());
+	public ResponseEntity<Categoria> buscarId(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
+
+	@GetMapping("/marcas/{marcas}")
+	public ResponseEntity<List<Categoria>> buscarMarcas(@PathVariable String marcas) {
+		return ResponseEntity.ok(repository.findAllByMarcasContainingIgnoreCase(marcas));
+	}
+
+	@GetMapping("/tipoProduto/{tipoProduto}")
+	public ResponseEntity<List<Categoria>> buscarTipoProduto(@PathVariable String tipoProduto) {
+		return ResponseEntity.ok(repository.findAllByTipoProdutoContainingIgnoreCase(tipoProduto));
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> postCategoria(@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+
 }
