5be89f7bf12b1408593dfb6796e4470ccbae6c1f
thomazfranca <trsf1990@gmail.com>
2021-07-05 11:11:08 -0300

    Post/Put/Delete (controller produto)

--- HABITAT/src/main/java/com/habitat/HABITAT/Controller/ProdutoController.java
+++ HABITAT/src/main/java/com/habitat/HABITAT/Controller/ProdutoController.java
@@ -3,12 +3,18 @@ package com.habitat.HABITAT.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.habitat.HABITAT.model.Produto;
 
@@ -37,5 +43,19 @@ public class ProdutoController {
 	public ResponseEntity<List<Produto>> buscarPreco(@PathVariable double preco) {
 		return ResponseEntity.ok(repository.findAllByPreco(preco));
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<Produto> postProduto (@RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> putCategoria (@RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.OK).body(produto);
+	}
+	
+	@DeleteMapping ("/id/{id}")
+		public void deletarProduto (@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
