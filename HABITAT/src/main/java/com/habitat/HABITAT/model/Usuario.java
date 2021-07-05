197faa420f07f0c36c254ce9a69c8992966eaf85
fegiacomelli <69607844+fegiacomelli@users.noreply.github.com>
2021-07-05 14:15:15 -0300

    Version 1.0.6

--- HABITAT/src/main/java/com/habitat/HABITAT/model/Usuario.java
+++ HABITAT/src/main/java/com/habitat/HABITAT/model/Usuario.java
@@ -1,11 +1,18 @@
 package com.habitat.HABITAT.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
@@ -37,6 +44,18 @@ public class Usuario {
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String senha;
+
+	@OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+	@JoinColumn(name = "fk_produto")
+	private List<Produto> produtosCriados = new ArrayList<>();
+	
+	public List<Produto> getProdutosCriados() {
+		return produtosCriados;
+	}
+
+	public void setProdutosCriados(List<Produto> produtosCriados) {
+		this.produtosCriados = produtosCriados;
+	}
 
 	public long getId() {
 		return id;
