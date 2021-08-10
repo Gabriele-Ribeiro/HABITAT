5a6baff9cbd388c2522e2cae937c6567c4b2f01f
fegiacomelli <69607844+fegiacomelli@users.noreply.github.com>
2021-08-10 13:28:25 -0300

    version 1.0.1

--- HABITAT_Front-End/app-ecom/src/app/produto/produto.component.ts
+++ HABITAT_Front-End/app-ecom/src/app/produto/produto.component.ts
@@ -5,6 +5,7 @@ import { environment } from 'src/environments/environment.prod';
 import { Categoria } from '../model/Categoria';
 import { Produto } from '../model/Produto';
 import { Usuario } from '../model/Usuario';
+import { CategoriaService } from '../service/categoria.service';
 import { ProdutoService } from '../service/produto.service';
 
 @Component({
@@ -16,18 +17,21 @@ export class ProdutoComponent implements OnInit {
    idCategoria:number
    categoria:Categoria = new Categoria 
    listaCategoria: Categoria[]
+
    produto: Produto = new Produto()
    listaProdutos: Produto[]
 
   constructor(
     private router: Router,
-    private produtoService: ProdutoService
+    private produtoService: ProdutoService,
+    private categoriaService: CategoriaService
   ) { }
 
   ngOnInit() {
 
  
   this.findAllProdutos()
+  this.findByIdCategoria()
 }
 
  findAllProdutos(){
@@ -37,9 +41,23 @@ this.listaProdutos = resp
 })
 }
 
+getAllCategoria(){
+    this.categoriaService.getAllCategoria().subscribe((resp: Categoria[])=>{
+      this.listaCategoria = resp
+    })
+}
+
+findByIdCategoria(){
+  this.categoriaService.getByIdCategoria(this.idCategoria).subscribe((resp: Categoria)=>{
+    this.categoria = resp
+  })
+  
+}
+
   cadastrar(){
     this.categoria.id = this.idCategoria
     this.produto.tipoMercadoria = this.categoria;
+
     console.log("produto"+JSON.stringify(this.produto))
       this.produtoService.postProduto(this.produto).subscribe((resp: Produto)=>{
     this.produto = resp
