287754cecb8ea7efb842d8e168a8e0f54a41a362
unknown <gabriele.ribeirot@gmail.com>
2021-08-10 14:39:13 -0300

    menu

--- HABITAT_Front-End/app-ecom/src/app/logar/logar.component.ts
+++ HABITAT_Front-End/app-ecom/src/app/logar/logar.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { UsuarioLogin } from '../model/UserLogin';
-import { Usuario } from '../model/Usuario';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -25,10 +24,8 @@ export class LogarComponent implements OnInit {
   }
 
   logar() {
-
     this.auth.logar(this.userLogin).subscribe((resp: UsuarioLogin) => {
       this.userLogin = resp
-
 
       environment.token = this.userLogin.token
       environment.nome = this.userLogin.nome
@@ -38,13 +35,12 @@ export class LogarComponent implements OnInit {
 
       this.router.navigate(['/inicio'])
     }, erro => {
+
       if (erro.status == 500) {
         alert('Usuário ou senha incorretos!')
       }
     }
-    )
-    }
-  
+  )} 
 }
 
 
